4fbc61caaa1baa8ad727ab2cf8d23ca59a7cba5c
Eleonore Breton <eleonore.breton@hec.edu>
2019-03-01 17:07:36 +0100

    js heroku

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -4,11 +4,13 @@ require("flatpickr/dist/themes/material_blue.css");
 
 const initFlatpickr = () => {
   const schedule = document.querySelector('#event_schedule')
-  flatpickr(schedule, {
-    enableTime: true,
-    altInput: true,
-    // dateFormat: "Y-m-d H:i",
+  if (schedule) {
+    flatpickr(schedule, {
+      enableTime: true,
+      altInput: true,
+      // dateFormat: "Y-m-d H:i",
   });
+  }
 }
 
 export { initFlatpickr };
